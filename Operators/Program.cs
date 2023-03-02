69d6385faa5b57f4f11d4c35db2bf3ec988ed082
aviladiogo <aviladiogo30@gmail.com>
2023-03-02 20:18:43 -0300

    Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Os operadores e a logica dos calculos são muito parecidos com o de outras linguagens de programação
+            byte a = 100;
+            byte b = 25;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            string sumString = $"The sum is {a + b}";
+            string subString = $"the subtraction is {a - b}";
+            string mulString = $"the multiplication is {a * b}";
+            string divString = $"the division is {a / b}";
+            Console.WriteLine(sumString);
+            Console.WriteLine(subString);
+            Console.WriteLine(mulString);
+            Console.WriteLine(divString);
+        }
+    }
+}
