2855bfa2b322b5c3616c177371bd42343cdd5337
aviladiogo <aviladiogo30@gmail.com>
2023-03-06 22:20:43 -0300

    Functions

---
+++ functions/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Os operadores e a logica dos calculos são muito parecidos com o de outras linguagens de programação
+            byte a = 5;
+            byte b = 3;
+            int sum = Sum(a, b);
+            int sub = Sub(a, b);
+            string sumString = $"The sum is {sum}";
+            Console.WriteLine(sumString);
+            string subString = $"The sum is {sub}";
+            Console.WriteLine(subString);
+
+            string sumsumsubString = $"The sum of the sum and the sub is {Sum(sum, sub)}";
+            Console.WriteLine(sumsumsubString);
+        }
+
+        private static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
